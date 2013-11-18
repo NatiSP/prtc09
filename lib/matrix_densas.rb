519774aeb89dbbb2edb8acd9e64e99f00977929f
Ramon Mesa <alu4429@cc417.(none)>
2013-11-18 13:13:23 +0000

    Implementación provisional de superclase Matrix y Matrix_Densa

---
+++ lib/matrix_densas.rb
@@ -0,0 +1,19 @@
+require 'matrix.rb'
+
+class Matrix_Densa < Matrix
+
+    def initialize(filas_d, columnas_d)
+      super
+	@matrix_d = Array.new(filas_d){Array.new(columnas_d, "0")}
+    end
+  
+    def [](filas_d, columnas_d)
+	@matrix_d[filas_d][columnas_d]
+    end
+
+    def []=(filas_d, columnas_d, elemento)
+	@matrix_d[filas_d][columnas_d] = elemento
+    end
+
+    
+    
